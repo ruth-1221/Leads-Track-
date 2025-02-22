3ac8c051c76201fb841a8f0320b6ec2ac9fd2891
ruth-1221 <ruthhirut01@gmail.com>
2025-02-21 22:48:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -41,8 +41,7 @@ onValue(referenceInDB, function(snapshot) {
     }
 })
 
-deleteBtn.addEventListener("dblclick", function() {
-    console.log("For Delete Please Double Click Delete Button")
+deleteBtn.addEventListener("click", function() {
     remove(referenceInDB)
     ulEl.innerHTML = ""
 })
